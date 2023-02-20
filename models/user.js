6988dd6e9b536f7dcbcf8b5ebfa8ff71a41b0cc9
Dharansh Neema <dharanshneema@gmail.com>
2023-02-20 12:17:33 +0530

    Completed user model file

--- models/user.js
+++ models/user.js
@@ -6,5 +6,38 @@ const userSchema = new mongoose.Schema({
     required: [true, "Name is required "],
     maxLenght: [40, "Name can't be greater than 40 character"],
   },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    validate: [validator.isEmail, "Please enter valid Email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    select: false,
+    minLength: [6, "Password should be of minimum length 6 "],
+  },
+  image: {
+    id: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: String,
+  createdAt: {
+    type: String,
+    default: Date.now,
+  },
 });
 module.exports = mongoose.model("user", userSchema);
