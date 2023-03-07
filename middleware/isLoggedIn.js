263f32a86b09e7a4f5c5daa7ccd1d381dfba1981
Dharansh Neema <dharanshneema@gmail.com>
2023-03-07 23:38:22 +0530

    isLoggedIn added

---
+++ middleware/isLoggedIn.js
@@ -0,0 +1,17 @@
+const BigPromise = require("../middleware/BigPromise");
+const user = require("../models/user");
+const jwt = require("jsonwebtoken");
+const customError = require("../utils/customError");
+const IsLoggedIn = BigPromise(async (req, res, next) => {
+  const token =
+    req.cookie.token ||
+    req.body.token ||
+    req.header("Authorization").replace("Bearer ", "");
+  if (!token) {
+    return next(new customError("Please Login first", 401));
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await user.findById(decoded);
+});
+
+module.exports = IsLoggedIn;
