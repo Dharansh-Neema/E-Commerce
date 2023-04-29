e69b89b96798f70e0e9a9b2198d252a539dce114
Dharansh Neema <dharanshneema@gmail.com>
2023-04-29 05:37:15 +0530

    Added product route: Now user can add products

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,5 +1,35 @@
-// const customError = require("../utils/customError");
+const customError = require("../utils/customError");
 const BigPromise = require("../middleware/BigPromise");
+//Importing cloudinary
+const cloudinary = require("cloudinary");
+
+//Importing product schema
+const product = require("../models/product");
+
+//Creating routes for product
+exports.addProduct = BigPromise(async (req, res, next) => {
+  let imageArray = [];
+  if (!req.files) {
+    return new customError("Product images are required", 401);
+  }
+  if (req.files) {
+    for (let index = 0; index < req.body.image.length; index++) {
+      const toUpload = req.files.image[index].tempFilePath;
+      const result = await cloudinary.v2.uploader.upload(toUpload, {
+        folder: "products",
+      });
+      imageArray.push({ id: result.public_id, securl_url: result.secure_url });
+    }
+  }
+  req.body.image = imageArray;
+  req.body.user = req.user.id;
+
+  const product = await product.create(req.body);
+  res.json({
+    success: true,
+    product,
+  });
+});
 
 exports.testProduct = BigPromise(async (req, res, next) => {
   res.status(200).json({
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const routers = express.Router();
-const { testProduct } = require("../controllers/productControllers");
+const {
+  testProduct,
+  addProduct,
+} = require("../controllers/productControllers");
 routers.route("/test").get(testProduct);
-
+routers.route("/addProduct").get(addProduct);
 module.exports = routers;
