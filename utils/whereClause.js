017118999b7680882db508c0adc5ee627d745c8e
Dharansh Neema <dharanshneema@gmail.com>
2023-04-29 15:39:56 +0530

    in the where clause filter and pager function added

--- utils/whereClause.js
+++ utils/whereClause.js
@@ -1,5 +1,6 @@
 //base - product.find()
 //bigQ - long string
+//TODO: Regex in js (to learn)
 class WhereCLause {
   constructor(base, bigQ) {
     this.base = base;
@@ -15,6 +16,31 @@ class WhereCLause {
         }
       : {};
     this.base = this.base.find({ ...searchword });
+    return this;
+  }
+  filter() {
+    //Craeting copy of bigQ
+    const copyQ = { ...this.bigQ };
+    // Deleting some of the non-required fileds
+    delete copyQ["limit"];
+    delete copyQ["page"];
+    delete copyQ["search"];
+
+    //Converting the copyQ into string
+    const stringCopyQ = JSON.stringify(copyQ);
+    stringCopyQ = stringCopyQ.replace(/\b(gte|lte|gt|lt)\b/g, (m) => `$${m}`);
+
+    const jsonofCopyQ = JSON.parse(stringCopyQ);
+    this.base = this.base.find(stringCopyQ);
+  }
+  Pager(resultPerPage) {
+    let currentPage = 1;
+    if (this.bigQ.page) {
+      currentPage = this.bigQ.page;
+    }
+    //IMP formula for skipping the values or skipping the pages
+    let skipVal = resultPerPage * (currentPage - 1);
+    this.base = this.base.limit(resultPerPage).skip(skipVal);
     return this;
   }
 }
